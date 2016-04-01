6389df940a1d166a6b23c9baa408a6890c1f971c
rajasjambekar <rajasjambekar@gmail.com>
2016-04-02 02:06:52 +0530

    Add task function

--- src/Loader.java
+++ src/Loader.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.*;
 import net.java.games.input.Controller;
 
@@ -10,18 +12,61 @@ public class Loader {
 	 * Responsible for initializing and launching the main UI 
 	 */
 	HashMap<String,Controller> connectedControllers;
+	Socket client;
 	
 	public Loader() {
 		initConnectedControllers();
+		try {
+			//startTcpConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
 		Loader load = new Loader();
 		(new DiscoverControllers(load.connectedControllers)).discover();
 		for(Map.Entry<String, Controller> entry:load.connectedControllers.entrySet()) {
-			if(entry.getValue().getType().toString()=="Stick" && entry.getValue().poll())
-				new Thread(new JoystickInputReader(new JoystickContainer(entry.getValue()))).start();
+			if(entry.getValue().getType().toString()=="Stick" && entry.getValue().poll()) {
+				JoystickContainer jc = new JoystickContainer(entry.getValue());
+				try {
+					addTasks(jc);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				new Thread(new JoystickInputReader(jc, load.client)).start();
+			}
 		}
+	}
+	
+	private static void addTasks(JoystickContainer jc) throws Exception {
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter tasks:");
+		String str = "";
+		while((str=buff.readLine()).contains(":")) {
+			String[] parts = str.split(":");
+			AxisTask task = new AxisTask(parts[0],parts[1],Integer.parseInt(parts[2]),Integer.parseInt(parts[3]));
+			jc.addAxisTask(task);
+			System.out.println("Enter tasks:");
+		}
+		AxisTask task = new AxisTask("CLKW","Maneuver",1300,0);
+		task.setToggleButtonNumber(1);
+		jc.addAxisTask(task);
+		task = new AxisTask("ACLK","Maneuver",1300,0);
+		task.setToggleButtonNumber(1);
+		jc.addAxisTask(task);
+	}
+
+	//start tcp connection
+	@SuppressWarnings("unused")
+	private void startTcpConnection() throws Exception {
+	   String serverName = "192.168.1.177";
+	   int port = 23;
+	   System.out.println("Connecting to " + serverName +" on port " + port);
+	   client = new Socket(serverName, port);
+	   System.out.println("Just connected to " + client.getRemoteSocketAddress());
 	}
 
 	//initialized the list of all controllers connected to the system
