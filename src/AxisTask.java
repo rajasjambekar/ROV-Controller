f3ced0bb041e8f4f3c318bcd7d1ddc0f23c1a2b0
rajasjambekar <rajasjambekar@gmail.com>
2016-04-01 23:42:46 +0530

    Replace Task with AxisTask and ButtonTask
    Common task class creates confusion and forces some controls to act in
    pre-defined way. Splitting helps in generalizing. Added in axis task are
    taskType and togglebutton which helps in generalizing some control tasks
    such as thrusters to a common type maneuvering. Toggle button is used to
    add multiple controls to same axis set.
    TaskType is also added to buttontask class

---
+++ src/AxisTask.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * Container class for storing task and mapping with an axis
+ * Object of this class is created by configuratorController class and stored by 
+ * any controller class (e.g. ManeuveringJoystick or roboticArmJoystick)
+ */
+public class AxisTask {
+	private String taskName;
+	private String taskType;
+	private int code;
+	private int axisNumber;  //x - 0, y - 1, z - 2
+	private int toggleButton = 0;
+	
+	public AxisTask(String taskName, String taskType, int  code, int axisNumber) {
+		this.taskName = taskName;  //name of task for user reference
+		this.taskType = taskType;  //type of task for user reference
+		this.code = code;   // Code used by arduino to identify component connected to arduino
+		this.axisNumber = axisNumber; //button number of the joystick holding object of this class
+	}
+	
+	//check if this task requires press of toggle button
+	public boolean containsToggleButton() {
+		if(toggleButton==0)
+			return false;
+		return true;
+	}
+	
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
+	}
+	
+	public void setcode(int  code) {
+		this.code = code;
+	}
+	
+	public void setAxisNumber(int axisNumber) {
+		this.axisNumber = axisNumber;
+	}
+	
+	public void setToggleButtonNumber(int toggleButtonNumber) {
+		this.toggleButton = toggleButtonNumber;
+	}
+	
+	public int getAxisNumber() {
+		return axisNumber;
+	}
+	
+	public int getToggleButtonNumber() {
+		return toggleButton;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public String getTaskName() {
+		return taskName;
+	}
+	
+	public String getTaskType() {
+		return taskType;
+	}
+}
--- src/ButtonTask.java
+++ src/ButtonTask.java
@@ -5,20 +5,28 @@ import java.util.*;
  * Container class for storing task and mapping with a button
  * Object of this class is created by configuratorController class and stored by 
  * any controller class (e.g. ManeuveringJoystick or roboticArmJoystick)
+ * If a task name is set to toggle, no other tasks can be coupled with this button
+ * One button may act as toggle for multiple tasks. 
  */
-public class Task {
+public class ButtonTask {
 	private String taskName;
+	private String taskType;
 	private int code;
 	private int buttonNumber;
 	
-	public Task(String taskName,int  code, int buttonNumber) {
+	public ButtonTask(String taskName, String taskType,int  code, int buttonNumber) {
 		this.taskName = taskName;  //name of task for user reference
+		this.taskType = taskType;  //type of task for user reference
 		this.code = code;   // Code used by arduino to identify component connected to arduino
 		this.buttonNumber = buttonNumber; //button number of the joystick holding object of this class
 	}
 	
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
+	}
+	
+	public void setTaskType(String taskType) {
+		this.taskType = taskType;
 	}
 	
 	public void setcode(int  code) {
@@ -39,5 +47,9 @@ public class Task {
 	
 	public String getTaskName() {
 		return taskName;
+	}
+	
+	public String getTaskType() {
+		return taskType;
 	}
 }
