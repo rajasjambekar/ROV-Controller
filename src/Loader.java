bd6689838c2153c6b70965bee35f896b9dcf49f2
rajasjambekar <rajasjambekar@gmail.com>
2016-04-05 03:15:26 +0530

    Not imp

--- src/Loader.java
+++ src/Loader.java
@@ -2,20 +2,26 @@ import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import net.java.games.input.Controller;
 
-public class Loader {
+public class Loader extends Application{
 
 	/*
 	 * Loader class is the starting point of this controller
 	 * Responsible for initializing the list of all connected controllers
 	 * Responsible for initializing and launching the main UI 
 	 */
-	HashMap<String,Controller> connectedControllers;
+	public HashMap<String,Controller> connectedControllers;
 	Socket client;
 	
 	public Loader() {
-		initConnectedControllers();
+		//initConnectedControllers();
+		//load.checkConnectedController();
 		try {
 			//startTcpConnection();
 		} catch (Exception e) {
@@ -26,8 +32,12 @@ public class Loader {
 	
 	public static void main(String[] args) {
 		Loader load = new Loader();
-		(new DiscoverControllers(load.connectedControllers)).discover();
-		for(Map.Entry<String, Controller> entry:load.connectedControllers.entrySet()) {
+		launch(args);
+	}
+	
+	private void checkConnectedController() {
+		//driver to test controller
+		for(Map.Entry<String, Controller> entry:connectedControllers.entrySet()) {
 			if(entry.getValue().getType().toString()=="Stick" && entry.getValue().poll()) {
 				JoystickContainer jc = new JoystickContainer(entry.getValue());
 				try {
@@ -36,7 +46,7 @@ public class Loader {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				new Thread(new JoystickInputReader(jc, load.client)).start();
+				new Thread(new JoystickInputReader(jc, client)).start();
 			}
 		}
 	}
@@ -47,8 +57,8 @@ public class Loader {
 		String str = "";
 		while((str=buff.readLine()).contains(":")) {
 			String[] parts = str.split(":");
-			AxisTask task = new AxisTask(parts[0],parts[1],Integer.parseInt(parts[2]),Integer.parseInt(parts[3]));
-			task.setToggleButtonNumber(Integer.parseInt(parts[4]));
+			AxisTask task = new AxisTask(parts[0],parts[1],Integer.parseInt(parts[2]),Integer.parseInt(parts[3]), Integer.parseInt(parts[4]));
+			
 			if(Integer.parseInt(parts[4])!=0) {
 				ButtonTask bTask = new ButtonTask("Toggle", parts[1], 0, Integer.parseInt(parts[4]));
 				jc.addButtonTask(bTask);
@@ -72,6 +82,28 @@ public class Loader {
 	//this list is a global controller list
 	private void initConnectedControllers() {
 		connectedControllers = new HashMap<String,Controller>();
+		//discover all controllers and add to list
+		(new DiscoverControllers(this.connectedControllers)).discover();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Team Screwdrivers: ROV Controller");
+        loadControllerGUI(primaryStage);
+	}
+
+	private void loadControllerGUI(Stage primaryStage) {
+		Parent root;
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(this.getClass().getResource("ControllerGUI.fxml"));
+			root = loader.load();
+			primaryStage.setScene(new Scene(root, 900, 600));
+			primaryStage.show();            
+            ControllerGUI controllerGUI = loader.getController();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
